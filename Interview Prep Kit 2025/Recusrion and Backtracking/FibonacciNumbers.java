a704e4e13713bce617f993bd8b67268657d26946
Vishal <vishal8995@gmail.com>
2025-04-21 21:48:42 +0530

    Tree & Recursion

---
+++ Interview Prep Kit 2025/Recusrion and Backtracking/FibonacciNumbers.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class FibonacciNumbers {
+
+    // Classic Recursion for smaller values of n
+    public static int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    // Dynamic Programming solution with memoization for larger values of n (Without
+    // Recursion)
+    // Time Complexity: O(n); Space Complexity: O(n)
+    public static int fibonacciON(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+
+        return dp[n];
+    }
+
+    // Dynamic Programming solution with memoization for larger values of n (Without
+    // Recursion)
+    // Time Complexity: O(n); Space Complexity: O(1)
+    public static int fibonacciO1(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return b;
+    }
+
+    // Dynamic Programming solution with memoization for larger values of n (With
+    // Recursion)
+    // O(n) Space; O(n) Time
+    public static int fibonacciRec(int n, int[] memo) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        if (memo[n] != -1)
+            return memo[n];
+
+        memo[n] = fibonacciRec(n - 1, memo) + fibonacciRec(n - 2, memo);
+        return memo[n];
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n)); // Recursion
+        System.out.println(fibonacciON(n)); // DP O(n) Space
+        System.out.println(fibonacciO1(n)); // DP O(1) Space
+
+        // DP with Recursion
+
+    }
+}
